caf9d6d4eb6312c13669f948c98bc878b70f90bb
Atilia <113184639+atiliaa2343@users.noreply.github.com>
2023-02-13 07:34:03 -0500

    Add files via upload
    Monopoly created using java

---
+++ first.java
@@ -0,0 +1,22 @@
+package First;
+import java.util.Scanner; 
+import java.util.Random;
+public class first { 
+      static void myMethod(){ 
+        double randomNumber = 0 + 7 * Math.random();
+        
+      }
+    public static void main(String[] args) {
+       System.out.print("Monopoly"); 
+       System.out.print("How to Play: Monopoly \n Monopoly can have 2-8 players in a game. Each player chooses a character to play and is given $1500 \n");
+       Scanner c = new Scanner(System.in); 
+       System.out.print("Choose a character: Top hat, Thimble, Iron, Shoe, Battleship, Cannon ");  
+       c.close();
+
+        
+        
+
+
+
+    }
+}
